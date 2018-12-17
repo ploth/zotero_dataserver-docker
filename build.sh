317a5c63d8f1543752eb931411b805fe1a22da6e
Pascal Loth <pascal.loth@tuhh.de>
2018-12-17 08:39:18 +0100

    Use forks to enable https

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-git clone --depth=1 git://git.27o.de/dataserver $DIR/dependencies/dataserver
-git clone --depth=1 git://git.27o.de/zss $DIR/dependencies/zss
+git clone --depth=1 https://gitlab.com/ploth/dataserver.git $DIR/dependencies/dataserver
+git clone --depth=1 https://gitlab.com/ploth/zss.git $DIR/dependencies/zss
 
 docker build $DIR -t zotero
